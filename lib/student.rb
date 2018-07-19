e6e1c769ed75bd30edd34912b80f4512ca4540c8
Alexandra Fren <alexandra.fren@gmail.com>
2018-07-19 21:49:55 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,12 +31,12 @@ class Student
     if self.id
       self.update
     else
-    sql = <<-SQL
-    INSERT INTO students(name, grade) VALUES (?,?)
-    SQL
-    DB[:conn].execute(sql, self.name, self.grade)
-  end
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+      sql = <<-SQL
+      INSERT INTO students(name, grade) VALUES (?,?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    end
   end
 
 end
