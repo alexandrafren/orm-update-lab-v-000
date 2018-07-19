554bba33311446646a52d8c7d9010343e9e5176d
Alexandra Fren <alexandra.fren@gmail.com>
2018-07-19 21:39:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,14 +2,14 @@ require_relative "../config/environment.rb"
 
 class Student
   attr_accessor :name, :grade
-  attr_reader :id 
+  attr_reader :id
 
   def initialize(name, grade, id = nil)
     @name = name
     @grade = grade
-    @id = id 
+    @id = id
   end
-  
+
 
 
 end
